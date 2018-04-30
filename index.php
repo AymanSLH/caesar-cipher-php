1c201951fa002c86c6031430b9d8276c130c0754
AymanSLH <ayman171salah@gmail.com>
2018-04-30 15:57:43 +0200

    Added Main file

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+
+function Cipher($ch, $key)
+{
+	if (!ctype_alpha($ch))
+		return $ch;
+
+	$offset = ord(ctype_upper($ch) ? 'A' : 'a');
+	return chr(fmod(((ord($ch) + $key) - $offset), 26) + $offset);
+}
+
+function Encipher($input, $key)
+{
+	$output = "";
+
+	$inputArr = str_split($input);
+	foreach ($inputArr as $ch)
+		$output .= Cipher($ch, $key);
+
+	return $output;
+}
+
+function Decipher($input, $key)
+{
+	return Encipher($input, 26 - $key);
+}
+
+// $text = "ayman Salah ";
+// $cipherText = Encipher($text, 3);
+// $plainText = Decipher($cipherText, 3)

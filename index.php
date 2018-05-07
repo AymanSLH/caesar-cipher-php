30e351b92ade7cbe0e8c2beb1f98edd782ce25fd
AymanSLH <ayman171salah@gmail.com>
2018-05-07 05:32:00 +0200

    Added interface and decrypt

--- index.php
+++ index.php
@@ -1,30 +1,74 @@
-<?php 
+<html>
+<head>
+<link rel="stylesheet" href="index.css">
 
-function Cipher($ch, $key)
-{
-	if (!ctype_alpha($ch))
-		return $ch;
+</head>
+        <body>
+        <form method="get" action="">
+            <h1>Caesar Text , Enter Text </h1>
+            <div class="bloop" id="blooper">
+                <input type="text" name="text">
+            </div>
+            <input type="submit" name="go" value="Go">
+        </form>
+        </body>
+                <?php
+                function encrypt($str, $offset) {
+                    $encrypted_text = "";
+                    $offset = $offset % 26;
+                    if($offset < 0) {
+                        $offset += 26;
+                    }
+                    $i = 0;
+                    while($i < strlen($str)) {
+                        $c = strtoupper($str{$i});
+                        if(($c >= "A") && ($c <= 'Z')) {
+                            if((ord($c) + $offset) > ord("Z")) {
+                                $encrypted_text .= chr(ord($c) + $offset - 26);
+                            } else {
+                                $encrypted_text .= chr(ord($c) + $offset);
+                            }
+                        } else {
+                            $encrypted_text .= " ";
+                        }
+                        $i++;
+                    }
+                    return $encrypted_text;
+                }
 
-	$offset = ord(ctype_upper($ch) ? 'A' : 'a');
-	return chr(fmod(((ord($ch) + $key) - $offset), 26) + $offset);
-}
+//                function decrypt($str, $offset) {
+//                    $decrypted_text = "";
+//                    $offset = $offset % 26;
+//                    if($offset < 0) {
+//                        $offset += 26;
+//                    }
+//                    $i = 0;
+//                    while($i < strlen($str)) {
+//                        $c = strtoupper($str{$i});
+//                        if(($c >= "A") && ($c <= 'Z')) {
+//                            if((ord($c) - $offset) < ord("A")) {
+//                                $decrypted_text .= chr(ord($c) - $offset + 26);
+//                            } else {
+//                                $decrypted_text .= chr(ord($c) - $offset);
+//                            }
+//                        } else {
+//                            $decrypted_text .= " ";
+//                        }
+//                        $i++;
+//                    }
+//                    return $decrypted_text;
+//                }
 
-function Encipher($input, $key)
-{
-	$output = "";
 
-	$inputArr = str_split($input);
-	foreach ($inputArr as $ch)
-		$output .= Cipher($ch, $key);
+                if (isset($_GET['go']))
+                    {
+                        $text  = $_GET['text'] ;
+                        $offset = 3 ;
+                        $new_text = encrypt($text , $offset) ;
+                        echo "The new Text is : " ; 
+                        echo "<br>" ;
+                        echo $new_text ;
+                    }
 
-	return $output;
-}
-
-function Decipher($input, $key)
-{
-	return Encipher($input, 26 - $key);
-}
-
-// $text = "ayman Salah ";
-// $cipherText = Encipher($text, 3);
-// $plainText = Decipher($cipherText, 3)
+                ?>
+</html>
